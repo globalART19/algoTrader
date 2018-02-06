555f499802254901b3f5b71668b332a3d10eea67
Andrew Trahan <globalart19@gmail.com>
2018-02-06 15:02:42 -0600

    Fixed DataFunc. Started DB pop func.

--- algoTrader/DataFunc.py
+++ algoTrader/DataFunc.py
@@ -5,59 +5,51 @@ import gdax, time
 
 mongo_client = pymongo.MongoClient()
 db = mongo_client.algodb_test
-BTC_collection = db.algoHistTable
+histData = db.algoHistTable
 
-# cur = BTC_collection.find().skip(1).limit(1)
-# print cur
-#
-# for doc in cur:
-#     print doc
+def calcPopulate(n):
+    
 
 
 # MACD 12 period moving average
-def msema(n):
+def m12ema(n):
     curSum = 0
-    new = BTC_collection.find().skip(n-11).limit(11)
-    first = BTC_collection.find().skip(n-12).limit(1)
-    print new
-    print first
+    if n < 12:
+        return
+    new = histData.find().skip(n-11).limit(11)
+    first = histData.find().skip(n-12).limit(1)
+#    print new
+#    print first
     for doc in new:
-        print doc
+#        print doc
         curVal = float(doc['hclose'])
         curSum = curSum + curVal
-    ema = (2 * first['hclose'] + curSum) / 13
-    print ema
-    return ema
-
-def msema2(n):
-    curSum = 0
-    new = BTC_collection.find().skip(n-11).limit(11)
-    first = BTC_collection.find_one().skip(n-12)
-    print new
-    print first
-    for doc in new:
-        print doc
-        curVal = new.get(doc['hclose'])
-        curSum = curSum + curVal
-    ema = (2 * first['hclose'] + curSum) / 13
-    print ema
+    for i in first:
+        firstVal = float(i['hclose'])
+    #print firstVal
+    ema = (2 * firstVal + curSum) / 13
+    #print ema
     return ema
 
 # MACD 26 period moving average
-def mlema(n):
-    new = BTC_collection.find().skip(n-25).limit(25)
-    first = BTC_collection.find().skip(n-26).limit(1)
+def m26ema(n):
+    curSum = 0
+    new = histData.find().skip(n-25).limit(25)
+    first = histData.find().skip(n-26).limit(1)
     for doc in new:
-        curVal = float(new["hclose"])
+        curVal = float(doc["hclose"])
         curSum = curSum + curVal
-    ema = (2 * float(first["hclose"]) + curSum) / 27
-    print ema
+    for i in first:
+        firstVal = float(i['hclose'])
+#    print firstVal
+    ema = (2 * firstVal + curSum) / 27
+#    print ema
     return ema
 
 # MACD average indicator
 def mave(n):
     ave = m12ema(n) - m26ema(n)
-    print ave
+#    print ave
     return ave
 
 # MACD signal 9 period moving average indicator
